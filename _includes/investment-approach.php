92269f6737bc9cee437b7ed2839f4794dbfa9ba5
sandesharanca <sandesh.mandavkar@aranca.com>
2017-02-13 16:54:56 +0530

    added cta module

--- _includes/investment-approach.php
+++ _includes/investment-approach.php
@@ -2,6 +2,7 @@
 include('_includes/masthead.html');
 include('_includes/process-steps-with-tabs.html');
 include('_includes/_list-element-4col-2row.html');
+include('_includes/_full-cta.html');
 include('_includes/contact.html');
 include('_includes/_CTA-form.html');	
 ?>
