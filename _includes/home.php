2a43bc37b12c3adb5dba06a1ee5070e75e01d756
sandesharanca <sandesh.mandavkar@aranca.com>
2017-02-13 17:47:53 +0530

    Designed new module

--- _includes/home.php
+++ _includes/home.php
@@ -3,6 +3,7 @@ include('_includes/masthead.html');
 include('_includes/landing.html');
 include('_includes/_fund-strategy.html');
 include('_includes/_intro-2col-2row.html');
+include('_includes/_why-aranca-c.html');
 include('_includes/contact.html');
 include('_includes/_CTA-form.html');
 ?>
