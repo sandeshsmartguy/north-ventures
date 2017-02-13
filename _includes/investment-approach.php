760924e889b02db0fa25dbf66d1f9f2eb6038e58
sandesharanca <sandesh.mandavkar@aranca.com>
2017-02-13 15:22:09 +0530

    added new page investment

---
+++ _includes/investment-approach.php
@@ -0,0 +1,7 @@
+<?php
+include('_includes/masthead.html');
+include('_includes/process-steps-with-tabs.html');
+include('_includes/_list-element-4col-2row.html');
+include('_includes/contact.html');
+include('_includes/_CTA-form.html');	
+?>
