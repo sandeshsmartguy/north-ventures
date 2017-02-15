db775e2c3f5a4c01a903e4a6bda2987bca443458
sandesharanca <sandesh.mandavkar@aranca.com>
2017-02-15 18:05:56 +0530

    updated homepage

--- _includes/home.php
+++ _includes/home.php
@@ -3,6 +3,10 @@ include('_includes/masthead.html');
 include('_includes/landing.html');
 include('_includes/_fund-strategy.html');
 include('_includes/_intro-2col-2row.html');
+include('_includes/_why-aranca-c.html');
+include('_includes/_content-row-grid.html');
+include('_includes/_sol-portfolio-b.html');
+include('_includes/_client-logos.html');
 include('_includes/contact.html');
 include('_includes/_CTA-form.html');
 ?>
