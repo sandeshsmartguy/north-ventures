d7b08afc525f5b6017374b133ff7752b97a7c71c
sandesharanca <sandesh.mandavkar@aranca.com>
2017-05-26 12:46:37 +0530

    create cms system 1 service page

--- mie/_include/_content.php
+++ mie/_include/_content.php
@@ -2,6 +2,8 @@
 
 if ( $_SERVER['REQUEST_URI'] == '/mie/' ) {
     include $content_file_path . DIRECTORY_SEPARATOR . 'home.php';
+} elseif ( $_SERVER['REQUEST_URI'] == '/services/visiting-faculty-onboarding-support-for-mba-institutions' ) {
+    include $content_file_path . DIRECTORY_SEPARATOR . 'visiting-faculty-onboarding-support-for-mba-institutions.php';
 } else {
     include $content_file_path . DIRECTORY_SEPARATOR . $_SERVER['REQUEST_URI'] . '.php';
 }
