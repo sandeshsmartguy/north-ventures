562d449332bc4c928691a31e800a63599e252d74
sandesharanca <sandesh.mandavkar@aranca.com>
2017-05-26 11:56:39 +0530

    make cms system

--- mie/_include/_content.php
+++ mie/_include/_content.php
@@ -9,7 +9,7 @@
 
         <?php
 
-        if ( $_SERVER['REQUEST_URI'] == '/' ) {
+        if ( $_SERVER['REQUEST_URI'] == '/mie' ) {
             include $content_file_path . DIRECTORY_SEPARATOR . 'home.php';
         } else {
             include $content_file_path . DIRECTORY_SEPARATOR . $_SERVER['REQUEST_URI'] . '.php';
