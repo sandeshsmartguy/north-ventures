9081e876cc8bbcfd78d59baebff520eb411a2ae4
sandesharanca <sandesh.mandavkar@aranca.com>
2017-05-26 11:50:02 +0530

    make cms system

---
+++ mie/_include/_content.php
@@ -0,0 +1,26 @@
+
+
+<!-- Page Content -->
+<div class="container">
+
+    <!-- Marketing Icons Section -->
+    <div class="row">
+
+
+        <?php
+
+        if ( $_SERVER['REQUEST_URI'] == '/' ) {
+            include $content_file_path . DIRECTORY_SEPARATOR . 'home.php';
+        } else {
+            include $content_file_path . DIRECTORY_SEPARATOR . $_SERVER['REQUEST_URI'] . '.php';
+        }
+
+        ?>
+
+    </div>
+
+    <?php // print_r($_SERVER['REQUEST_URI']); ?>
+    <!-- /.row -->
+
+</div>
+<!-- / page container  -->
