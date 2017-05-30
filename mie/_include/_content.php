1ec12430e4cb557710e7d5db11e369245a7c197e
sandesharanca <sandesh.mandavkar@aranca.com>
2017-05-30 17:57:14 +0530

    updated visiting faculty service page

--- mie/_include/_content.php
+++ mie/_include/_content.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( $_SERVER['REQUEST_URI'] == '/mie/' ) {
-    include $content_file_path . DIRECTORY_SEPARATOR . 'home.php';
+    include $content_file_path . DIRECTORY_SEPARATOR . 'visiting-faculty-onboarding-support-for-mba-institutions.php';
 } else {
     include $content_file_path . DIRECTORY_SEPARATOR . $_SERVER['REQUEST_URI'] . '.php';
 }
